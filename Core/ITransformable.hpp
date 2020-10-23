31b568b996391278a70bfcef1bcb654e69a95393
El Strategico <strategico.dev@gmail.com>
2020-10-24 02:38:15 +0800

    Added ITransformable

---
+++ Core/ITransformable.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace NovaRPG
+{
+	class ITransformable
+	{
+	public:
+		virtual sf::FloatRect getSize() = 0;
+		virtual void setSize(const sf::Vector2f& size) = 0;
+
+		virtual sf::Vector2f getPosition() = 0;
+		virtual void move(const sf::Vector2f& position) = 0;
+		virtual void setPosition(const sf::Vector2f& position) = 0;
+	};
+}

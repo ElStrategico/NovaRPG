42faabe7c9d4d42bbf1b342c68b19249af8a670f
El Strategico <strategico.dev@gmail.com>
2020-10-24 02:42:26 +0800

    Added Text

---
+++ UI/Text.cpp
@@ -0,0 +1,58 @@
+#include "Text.hpp"
+
+NovaRPG::Text::Text(
+	const std::string& text,
+	uint32_t size,
+	const sf::Vector2f& position,
+	const sf::Color& color,
+	const std::string& font
+)
+{
+	this->font.loadFromFile(font);
+
+	this->text.setString(text);
+	this->text.setCharacterSize(size);
+	this->text.setPosition(position);
+	this->text.setFillColor(color);
+	this->text.setFont(this->font);
+}
+
+void NovaRPG::Text::setCharacterSize(uint32_t size)
+{
+	text.setCharacterSize(size);
+}
+
+void NovaRPG::Text::setColor(const sf::Color& color)
+{
+	text.setFillColor(color);
+}
+
+sf::FloatRect NovaRPG::Text::getSize()
+{
+	return text.getLocalBounds();
+}
+
+void NovaRPG::Text::setSize(const sf::Vector2f& size)
+{
+	//TODO: Implement
+}
+
+sf::Vector2f NovaRPG::Text::getPosition()
+{
+	return text.getPosition();
+}
+
+void NovaRPG::Text::move(const sf::Vector2f& position)
+{
+	text.move(position);
+}
+
+void NovaRPG::Text::setPosition(const sf::Vector2f& position)
+{
+	text.setPosition(position);
+}
+
+void NovaRPG::Text::draw(sf::RenderWindow* window)
+{
+	window->draw(text);
+}
---
+++ UI/Text.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+#include <SFML/Graphics.hpp>
+
+#include <UI/Element.hpp>
+
+namespace NovaRPG
+{
+	class Text : public Element
+	{
+	private:
+		sf::Text text;
+		sf::Font font;
+	public:
+		Text() {}
+		Text(
+			const std::string& text,
+			uint32_t size = 30,
+			const sf::Vector2f& position = sf::Vector2f(0.0f, 0.0f),
+			const sf::Color& color = sf::Color::White,
+			const std::string& font = "Assets/Fonts/Font.ttf"
+		);
+
+		void setCharacterSize(uint32_t size);
+		void setColor(const sf::Color& color);
+
+		virtual sf::FloatRect getSize() override;
+		virtual void setSize(const sf::Vector2f& size) override;
+
+		virtual sf::Vector2f getPosition() override;
+		virtual void move(const sf::Vector2f& position) override;
+		virtual void setPosition(const sf::Vector2f& position) override;
+
+		virtual void draw(sf::RenderWindow* window) override;
+	};
+}

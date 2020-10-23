b031a5bc766d41897ab5dae07c45ab3cfb46291a
El Strategico <strategico.dev@gmail.com>
2020-10-24 03:47:18 +0800

    Added logic for create text box

--- Factories/LoginMenuFactory.cpp
+++ Factories/LoginMenuFactory.cpp
@@ -4,10 +4,17 @@ int NovaRPG::LoginMenuFactory::itemCharacterSize = 50;
 
 NovaRPG::Menu* NovaRPG::LoginMenuFactory::factory()
 {
+	TextBox* login = new TextBox(sf::Vector2f(300.0f, 30.0f));
 	Button* loginButton = new Button("Login", itemCharacterSize);
+	Button* mainMenuButton = new Button("To main menu", itemCharacterSize);
+
+	EventController::registry(EventType::ON_CLICK, loginButton, onLoginClick());
+	EventController::registry(EventType::ON_CLICK, mainMenuButton, onMainMenuClick());
 
 	std::vector<Element*> elements = {
-		loginButton
+		login,
+		loginButton,
+		mainMenuButton
 	};
 
 	sf::Vector2f position(
@@ -16,4 +23,18 @@ NovaRPG::Menu* NovaRPG::LoginMenuFactory::factory()
 	);
 
 	return new Menu(elements, position);
+}
+
+std::function<void(NovaRPG::Event&)> NovaRPG::LoginMenuFactory::onLoginClick()
+{
+	return [](Event& event) {
+		std::cout << "Login" << std::endl;
+	};
+}
+
+std::function<void(NovaRPG::Event&)> NovaRPG::LoginMenuFactory::onMainMenuClick()
+{
+	return [](Event& event) {
+		SceneController::switchScene(new MainMenu());
+	};
 }
--- Factories/LoginMenuFactory.hpp
+++ Factories/LoginMenuFactory.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <functional>
+
 #include <UI/Menu.hpp>
 #include <UI/Button.hpp>
+#include <UI/TextBox.hpp>
+#include <Scenes/MainMenu.hpp>
 #include <Core/GameSettings.hpp>
+#include <Scenes/SceneController.hpp>
+#include <EventSystem/EventController.hpp>
 
 namespace NovaRPG
 {
@@ -10,6 +17,9 @@ namespace NovaRPG
 	{
 	private:
 		static int itemCharacterSize;
+
+		static std::function<void(Event&)> onLoginClick();
+		static std::function<void(Event&)> onMainMenuClick();
 	public:
 		static Menu* factory();
 	};

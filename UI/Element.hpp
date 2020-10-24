157f9a111f2bd09d040e87bdb8e84c3d0665c29a
El Strategico <strategico.dev@gmail.com>
2020-10-24 22:14:34 +0800

    Refactoring

--- UI/Element.hpp
+++ UI/Element.hpp
@@ -4,12 +4,11 @@
 
 #include <SFML/Graphics.hpp>
 
-#include <Core/IDrawable.hpp>
 #include <Core/GameObject.hpp>
 
 namespace NovaRPG
 {
-	class Element : public GameObject, public IDrawable
+	class Element : public GameObject
 	{
 	public:
 		virtual ~Element();

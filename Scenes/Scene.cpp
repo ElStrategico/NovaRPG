2d344acd9ad050e474f0ca1863776bcd55333516
El Strategico <strategico.dev@gmail.com>
2020-10-24 22:14:01 +0800

    Added logic for work with GameObject

--- Scenes/Scene.cpp
+++ Scenes/Scene.cpp
@@ -5,12 +5,44 @@ NovaRPG::Scene::Scene(const std::string& name) : name(name)
     
 }
 
+NovaRPG::Scene::~Scene()
+{
+	for (auto& gameObject : gameObjects)
+	{
+		if (gameObject) delete gameObject;
+	}
+}
+
 std::string NovaRPG::Scene::getName()
 {
     return name;
 }
 
+void NovaRPG::Scene::addGameObject(GameObject* gameObject)
+{
+    gameObjects.push_back(gameObject);
+}
+
+NovaRPG::GameObject* NovaRPG::Scene::findGameObjectByTag(const std::string& tag)
+{
+	for (auto& gameObject : gameObjects)
+	{
+		if (gameObject->existsTag(tag))
+		{
+			return gameObject;
+		}
+	}
+
+	return nullptr;
+}
+
 void NovaRPG::Scene::draw(sf::RenderWindow* window)
 {
-
+	for (auto& gameObject : gameObjects)
+	{
+		if (gameObject->isActive())
+		{
+			gameObject->draw(window);
+		}
+	}
 }
--- Scenes/Scene.hpp
+++ Scenes/Scene.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include <Core/IDrawable.hpp>
+#include <Core/GameObject.hpp>
 
 namespace NovaRPG
 {
@@ -10,11 +12,17 @@ namespace NovaRPG
 	{
 	private:
 		std::string name;
+	protected:
+		std::vector<GameObject*> gameObjects;
 	public:
 		Scene(const std::string& name);
-		virtual ~Scene() {}
+		virtual ~Scene();
 
 		std::string getName();
+
+		void addGameObject(GameObject* gameObject);
+
+		GameObject* findGameObjectByTag(const std::string& tag);
 
 		virtual void draw(sf::RenderWindow* window) override;
 	};

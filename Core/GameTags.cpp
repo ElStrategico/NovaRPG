d12bf80611e2eb00bfa7a61424628302945bb39f
El Strategico <strategico.dev@gmail.com>
2020-10-24 22:11:45 +0800

    Added GameTags

---
+++ Core/GameTags.cpp
@@ -0,0 +1,5 @@
+#include "GameTags.hpp"
+
+std::string NovaRPG::GameTags::loginInput = "LoginInput";
+std::string NovaRPG::GameTags::passwordInput = "PasswordInput";
+std::string NovaRPG::GameTags::loginMenu = "LoginMenu";
---
+++ Core/GameTags.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace NovaRPG
+{
+	class GameTags
+	{
+	public:
+		static std::string loginInput;
+		static std::string passwordInput;
+		static std::string loginMenu;
+	};
+}

1f928510d7d2c5cdfdac16948577b77d35517fb3
El Strategico <strategico.dev@gmail.com>
2020-10-24 22:12:40 +0800

    Added logic for work with tag

--- Core/GameObject.cpp
+++ Core/GameObject.cpp
@@ -7,6 +7,49 @@ NovaRPG::GameObject::~GameObject()
 	EventController::deleteForGameObject(this);
 }
 
+void NovaRPG::GameObject::addTag(const std::string& name)
+{
+	addTag(Tag(name));
+}
+
+void NovaRPG::GameObject::addTag(const Tag& tag)
+{
+	tags.push_back(tag);
+}
+
+bool NovaRPG::GameObject::existsTag(const std::string& name)
+{
+	for (auto& tag : tags)
+	{
+		if (tag.getName() == name)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+std::vector<NovaRPG::Tag> NovaRPG::GameObject::getTags()
+{
+	return tags;
+}
+
+std::string NovaRPG::GameObject::getValue()
+{
+	return value;
+}
+
+void NovaRPG::GameObject::setValue(const std::string& value)
+{
+	this->value = value;
+}
+
+void NovaRPG::GameObject::appendValue(const std::string& value)
+{
+	this->value += value;
+}
+
 bool NovaRPG::GameObject::isActive()
 {
 	return active;
--- Core/GameObject.hpp
+++ Core/GameObject.hpp
@@ -1,17 +1,33 @@
 #pragma once
 
+#include <vector>
 #include <iostream>
 
+#include <Core/Tag.hpp>
+#include <Core/IDrawable.hpp>
 #include <Core/ITransformable.hpp>
 
 namespace NovaRPG
 {
-	class GameObject : public ITransformable
+	class GameObject : public IDrawable, public ITransformable
 	{
 	private:
 		bool active = true;
+		std::vector<Tag> tags;
+	protected:
+		std::string value;
 	public:
 		virtual ~GameObject();
+
+		void addTag(const std::string& name);
+		void addTag(const Tag& tag);
+
+		bool existsTag(const std::string& name);
+		std::vector<Tag> getTags();
+
+		virtual std::string getValue();
+		virtual void setValue(const std::string& value);
+		void appendValue(const std::string& value);
 
 		bool isActive();
 		void setActive(bool value);

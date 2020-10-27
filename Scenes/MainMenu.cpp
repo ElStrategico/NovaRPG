a129613ea561af60681fcc3dca4a20cc0362cba3
El Strategico <strategico.dev@gmail.com>
2020-10-28 00:15:20 +0800

    Added logic for work with factoryProfileMenu

--- Scenes/MainMenu.cpp
+++ Scenes/MainMenu.cpp
@@ -4,4 +4,5 @@ NovaRPG::MainMenu::MainMenu() : Scene("MainMenu")
 {
 	addGameObject(TileFactory::factoryFullScreen("Assets/MainMenu/Background.png"));
 	addGameObject(MainMenuFactory::factory());
+	addGameObject(MainMenuFactory::factoryProfileMenu());
 }

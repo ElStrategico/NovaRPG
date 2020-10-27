bd1a4d9840bca3e71e4115914cdf452e0a0de7bb
El Strategico <strategico.dev@gmail.com>
2020-10-28 00:14:06 +0800

    Added method user, getName

--- Network/Auth.cpp
+++ Network/Auth.cpp
@@ -2,5 +2,23 @@
 
 bool NovaRPG::Auth::isUser()
 {
-    return false;
+    return true;
+}
+
+NovaRPG::User NovaRPG::Auth::user()
+{
+    sf::Http::Request request("/api/v1/auth/me", sf::Http::Request::Get);
+    sf::Http::Response response = HTTPClient::sendWithApplicationJSON(request);
+
+    std::string name = response.getBody();
+
+    return User(name);
+}
+
+std::string NovaRPG::Auth::getName()
+{
+    sf::Http::Request request("/api/v1/auth/me", sf::Http::Request::Get);
+    sf::Http::Response response = HTTPClient::sendWithApplicationJSON(request);
+
+    return response.getBody();
 }
--- Network/Auth.hpp
+++ Network/Auth.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <iostream>
+
+#include <Models/User.hpp>
+#include <Network/HTTPClient.hpp>
+
 namespace NovaRPG
 {
 	class Auth
 	{
 	public:
 		static bool isUser();
+		static User user();
+		static std::string getName();
 	};
 }
